863890832f6385c87180813926251e3b15a6070b
JoaoTeramatsu <joaovictor.teramatsu@gmail.com>
2022-08-26 00:44:30 -0300

    Trabalho - PostgreSQL

--- TI2 - PostgreSQL/src/main/java/dao/DAO.java
+++ TI2 - PostgreSQL/src/main/java/dao/DAO.java
@@ -14,7 +14,7 @@ public class DAO {
 	public boolean conectar() {
 		String driverName = "org.postgresql.Driver";                    
 		String serverName = "localhost";
-		String mydatabase = "teste";
+		String mydatabase = "TeramatsuDB";
 		int porta = 5432;
 		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
 		String username = "joaoteramatsu";
